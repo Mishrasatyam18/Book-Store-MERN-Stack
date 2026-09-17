17b3d2cf6cd7ec5475b4fa96a2477ddf3a13ed12
Satyam Mishra <mishrasatyam772526@gmail.com>
2026-09-17 13:44:06 +0530

    Prepare app for Render deployment

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -4,6 +4,7 @@ import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
+import { API_URL } from '../config';
 
 const CreateBooks = () => {
   const [title, setTitle] = useState('');
@@ -21,7 +22,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post(`${API_URL}/books`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book created successfully', {
